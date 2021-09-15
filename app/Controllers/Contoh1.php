cefe2466a89f7562f526afdf1fad3fa2a9c4b156
Muzaqi Hatami <muzaqi.hatami@amarbank.co.id>
2021-09-16 01:54:41 +0700

    inisialisasi code igniter awal dan membuat tampilan biodata diri pada controller contoh1

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "Perkenalkan<br/>";
+        echo "Nama Saya Muzaqi Hatami, saya mahasiswa Universitas Bina Sarana Informatika<br/>";
+        echo "saya memiliki hobi bermain game";
+    }
+}
